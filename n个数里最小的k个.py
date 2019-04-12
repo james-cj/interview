a7f01db95d732daecd5ec2c37dfe2cb04d0277d3
James-xm <49548268+james-xm@users.noreply.github.com>
2019-04-12 16:03:48 +0800

    Update n个数里最小的k个.py

--- n个数里最小的k个.py
+++ n个数里最小的k个.py
@@ -4,3 +4,13 @@ a) 逐个返回数字，输出在同一行
 b) 逐个返回字符串，输出在同一行
 c) 返回一个包含k个最小数的数组
 """
+x = [int(i) for i in input().split()]
+k = x[-1]
+x.remove(x[-1])
+x.sort()
+print(' '.join(list(map(str,x[:k]))))
+
+
+# a = list(map(int, input().split()))
+# lists, k = a[:-1], a[-1]
+# print(" ".join(list(map(str, sorted(lists)[:k]))))
